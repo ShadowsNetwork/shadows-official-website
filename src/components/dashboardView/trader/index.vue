1eb73464a33825a9a38f1194934ed817f58eff8a
Patrick Kim <patrick.y.kim@gmail.com>
2021-03-29 13:40:49 -0700

    Home - assets section mobile responsiveness and replace image

--- src/components/dashboardView/trader/index.vue
+++ src/components/dashboardView/trader/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="trader">
+    <div class="pc-content">
     <div class="line"/>
     <div class="trader-title wow bounceInUp" data-wow-duration="1.5s" data-wow-delay="0.3s">
       <span>Investments for</span>
       <span>Every Trader</span>
     </div>
+
     <div class="traderText wow slideInRight" data-wow-duration="1.5s" data-wow-delay="0.3s">
       <span>Asset Classes</span>
       <span>Invest in a wide variety of derivative asset classes: Fiat Currencies (USD, EUR), Crypto Currencies (BTC, ETH), Commodities (GOLD, SILVER), Equities (TSLA, AAPL), and many more.</span>
     </div>
-    <img class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.3s" src="../../../assets/image/dashboard/groupAssets.png">
+    
+    <div class="wow bounceIn globe" data-wow-duration="1.5s" data-wow-delay="0.3s" >
+      
+    </div>
+    
+    </div>
   </div>
 </template>
 
@@ -26,6 +33,22 @@ export default {
   width: 100%;
   position: relative;
 }
+.pc-content {
+  max-width: 1600px;
+  margin: auto;
+  display: block;
+}
+
+
+ .globe {
+    background: url("../../../assets/image/dashboard/groupAssets.png") center;
+    margin-top: 10px;
+    margin-bottom:90px;
+    background-repeat: no-repeat;
+    height: 1000px;
+    width: 80%;
+  }
+
 .line {
   width: 80%;
   height: 2px;
@@ -52,10 +75,10 @@ export default {
   margin-left: 5%;
 }
 .traderText {
-  width: 20%;
+  width: 300px;
   position: absolute;
   top: 48%;
-  right: 15%;
+  right: 30%;
 }
 .traderText > span:nth-of-type(1) {
   display: block;
@@ -73,36 +96,100 @@ export default {
   color: #fff;
   margin-top: 10px;
 }
-@media screen and (max-width: 900px){
-  .trader-title {
-    position: relative;
-    top: 40px;
+
+@media screen and (max-width: 3400px){
+  
+  .traderText {
+    width: 300px;
+    position: absolute;
+    top: 48%;
+    right: 30%;
   }
-  .trader-title > span {
-    font-size: 16px;
+  
+}
+
+@media screen and (max-width: 2800px){
+  
+  .traderText {
+    width: 300px;
+    position: absolute;
+    top: 48%;
+    right: 30%;
+  }
+  
+}
+
+@media screen and (max-width: 2200px){
+  
+  .traderText {
+    width: 300px;
+    position: absolute;
+    top: 48%;
+    right: 20%;
+  }
+  
+}
+
+@media screen and (max-width: 1600px){
+  
+  .traderText {
+    width: 300px;
+    position: absolute;
+    top: 48%;
+    right: 10%;
+  }
+  
+}
+
+@media screen and (max-width: 1200px){
+  
+  .traderText {
+    width: 300px;
+    position: absolute;
+    top: 48%;
+    right: 5%;
+  }
+  
+}
+
+@media screen and (max-width: 1100px){
+  
+  .traderText {
+    width: 300px;
+    position: absolute;
+    top: 48%;
+    right: 0%;
+  }
+  
+}
+
+
+@media screen and (max-width: 900px){
+  .globe {
+    margin-top: 10px;
+    margin-bottom:90px;
+    background: url("../../../assets/image/dashboard/groupAssets.png") center;
+    width: 100%;
   }
   .traderText {
-    width: 30%;
+    width: 300px;
     position: absolute;
-    top: 30%;
-    right: 4%;
+    top: 80%;
+    right: 5%;
   }
-  .traderText > span:nth-of-type(1) {
-    font-size: 16px;
-  }
-  .traderText > span:nth-of-type(2) {
-    font-size: 12px;
-  }
-  .trader > img {
-  width: 70%;
-  margin-left: 5%;
-}
 }
 
 @media screen and (max-width: 450px){
   .trader-title { 
     position: relative;
     top: 80px;
+  }
+  .globe {
+    margin-top: 10px;
+    margin-bottom:90px;
+    background: url("../../../assets/image/dashboard/groupAssetsMobile.png") center;
+    height: 600px;
+    width: 100%;
   }
   .trader-title > span {
     font-family: Arboria-Bold;
@@ -114,7 +201,7 @@ export default {
     padding-right: 20%;
     text-align: center;
     position: absolute;
-    top: 70%;
+    top: 550px;
     right: 0%;
   }
   .traderText > span:nth-of-type(1) {
